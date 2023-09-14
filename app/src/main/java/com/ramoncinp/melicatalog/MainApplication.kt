663c78def02f59fa755ead7553eb539e2b3525d7
aldairparra <aldairparrag@gmail.com>
2023-09-13 22:29:22 -0600

    Add Hilt and Timber

---
+++ app/src/main/java/com/ramoncinp/melicatalog/MainApplication.kt
@@ -0,0 +1,18 @@
+package com.ramoncinp.melicatalog
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(
+                Timber.DebugTree()
+            )
+        }
+    }
+}
